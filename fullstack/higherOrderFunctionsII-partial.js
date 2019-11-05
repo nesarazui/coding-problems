beed6ba181eba1f0575dc7bf514e025b08b9cd45
nesarazui <56412398+nesarazui@users.noreply.github.com>
2019-11-05 16:08:13 -0500

    Create higherOrderFunctionsII-partial.js

---
+++ fullstack/higherOrderFunctionsII-partial.js
@@ -0,0 +1,21 @@
+Write a function, partial, that accepts a callback and one argument (let's call it 'arg a'). Partial should return a new function. When invoked, the new function will:
+
+Accept its own argument (let's call it 'arg b')
+Call the callback, passing in 'arg a' as the first argument and 'arg b' as the second argument
+Return the value returned by the callback
+function summer (a, b) {
+  return a + b
+};
+
+let sumFive = partial(summer, 5);
+sumFive(10) // => 15;
+
+// YOUR CODE BELOW
+const partial = (callback, argA) => {
+  
+  const func = (argB) => {
+    return callback(argA, argB)
+  }
+
+  return func
+}
