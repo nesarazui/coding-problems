f3fe903a4eb7eecaf023c43e70c2e5c76b68f4cb
nesarazui <56412398+nesarazui@users.noreply.github.com>
2019-11-05 16:54:50 -0500

    Create project-whats-for-dinner-khan-academy.js

---
+++ fullstack/project-whats-for-dinner-khan-academy.js
@@ -0,0 +1,38 @@
+//design a 'what's for dinner' plate by using shapes and colors
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+fill(0, 196, 255);
+ellipse(200, 200, 300, 300); 
+
+// box of sweets
+noStroke();
+fill(204, 255, 0);
+rect(80, 200, 50, 10);
+
+// meatballs
+fill(102, 4, 4);
+ellipse(250, 200, 25, 25);
+ellipse(220, 200, 20, 20);
+ellipse(275, 175, 40, 40);
+
+//arc food yum
+strokeWeight(5);
+stroke(46, 36, 46);
+fill(194, 10, 71);
+arc(200, 280, 178, 121, 0, 180);
+
+// watermelon seeds
+noStroke();
+fill(219, 144, 219);
+ellipse(144, 300, 20, 20);
+ellipse(244, 300, 10, 10);
+ellipse(200, 330, 10, 10);
+ellipse(175, 290, 10, 10);
+ellipse(212, 310, 10, 10);
+
+// triangle brocolli
+noStroke();
+fill(9, 242, 102);
+triangle(140, 140, 200, 100, 260, 140);
+
